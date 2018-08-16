9b9ef31b1c94abef58561c039212e544a32c9834
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2018-08-16 21:22:28 +0700

    initial project

---
+++ src/main/java/com/bcorpse/multimodule/MultiModuleApplication.java
@@ -0,0 +1,12 @@
+package com.bcorpse.multimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleApplication.class, args);
+	}
+}
